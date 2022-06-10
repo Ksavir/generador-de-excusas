f83052199e319f07ab74a0876848cfc12059050c
Ksavir <kevinsavir20@gmail.com>
2022-06-10 01:42:48 +0000

    excuse generator excercise

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function excuse() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoExc = Math.floor(Math.random() * who.length);
+  let actionExc = Math.floor(Math.random() * action.length);
+  let whatExc = Math.floor(Math.random() * what.length);
+  let whenExc = Math.floor(Math.random() * when.length);
+
+  document.querySelector("p").innerHTML =
+    who[whoExc] +
+    " " +
+    action[actionExc] +
+    " " +
+    what[whatExc] +
+    " " +
+    when[whenExc];
 };
